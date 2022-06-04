e6b334d101bbd2a70cbe37d926af078999cccf17
gangslee <grandnet1225@gmail.com>
2022-06-04 23:35:31 +0900

    [관심종목] Response Data 구성 수정

--- src/controllers/stockController.ts
+++ src/controllers/stockController.ts
@@ -9,10 +9,8 @@ export const getPriceByTicker = async (req: Request, res: Response) => {
   const { regularMarketPrice, regularMarketChangePercent } = await yahooFinance.quote(`${ticker}`);
 
   return res.send({
-    data: {
-      price: regularMarketPrice,
-      rate: regularMarketChangePercent?.toFixed(2)
-    }
+    price: regularMarketPrice,
+    rate: regularMarketChangePercent?.toFixed(2)
   });
 }
 
@@ -25,7 +23,7 @@ export const getExchangeRate = async (req: Request, res: Response) => {
 export const getSingleHistory = async (req: Request, res: Response) => {
   const { ticker, period } = req.params;
 
-  const queryOptions = { period1: getDay(parseInt(period)), period2: getDay(0) };
+  const queryOptions = { period1: getDay(-parseInt(period)), period2: getDay(0) };
   const result = await yahooFinance.historical(`${ticker}`, queryOptions);
 
   return res.send({ data: { result, cnt: result.length } });
